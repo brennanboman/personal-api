4bd713a22d81b11dd4467a96e3795a923228c14b
Brennan Boman <brennanboman@Brennans-MacBook-Air.local>
2017-03-09 19:27:35 -0700

    first save

---
+++ user.js
@@ -0,0 +1,53 @@
+var user = {
+  name: 'Brennan Boman',
+  location: 'West Jordan',
+  occupations: ['Client Representative'],
+  hobbies: [
+    {
+      name: 'Golf',
+      type: 'Sport'
+    },
+    {
+      name: 'Video Games',
+      type: 'eSport'
+    },
+    {
+      name: 'Utah Jazz/Real Salt Lake',
+      type: 'Fan'
+    }
+  ],
+  family: [
+    {
+      name: 'Shonika Boman',
+      relation: 'Spouse',
+      gender: 'Female'
+    },
+    {
+      name: 'LaDawn Boman',
+      relation: 'Mom',
+      gender: 'Female'
+    },{
+      name: 'Michael Boman',
+      relation: 'Dad',
+      gender: 'Male'
+    }
+  ],
+  restaurants: [
+    {
+      name: 'La Fountain',
+      type: 'Mexican',
+      rating: 9
+    },
+    {
+      name: 'Charlie Chows',
+      type: 'Chinese',
+      rating: 10
+    },
+    {
+      name: 'Sushi Monster',
+      type: 'Sushi',
+      rating: 9
+    }
+  ]
+};
+module.exports = user;
